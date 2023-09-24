18629c69dc75d9c204e5dd180d83969f97f8254b
coldnanee <pmihaylov036@gmail.com>
2023-09-24 18:51:23 +0300

    init project

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Providers from "./providers";
+import "../src/styles/index.scss";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<Providers>
+		<div></div>
+	</Providers>
+);
